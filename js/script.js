6018d6b855050591143db5ec61abf66dd43e4edb
Rino Nakagawara <nakagawara@reclick.co.jp>
2020-12-24 00:27:38 +0900

    feat: portfolioの作成

--- js/script.js
+++ js/script.js
@@ -2,18 +2,14 @@
 initPhotoSwipeFromDOM(".js-my-gallery");
 
 $(function () {
-
   //iOS対策
-  //iOSでは疑似要素を含むaタグのリンクは２回タップしないと遷移とページ内スクロールをしないため、
-  //ユーザーエージェント判定でiOSの場合はbodyタグにiosを付与し、対象の疑似要素をdisplay: noneする
   var ua = navigator.userAgent;
   if (/iPhone|iPad|iPod/.test(ua)) {
     $("body").addClass("ios");
   }
 
   //Worksのリンクを有効化
-  //スライド（Swiper）内に記載のリンクを有効にするため下記の記述が必要 (;´･ω･)ｳｰﾝ･･･
-  $(".works-url").on("click", "a", function (e) {
+  $(".works-url, .works-title").on("click", "a", function (e) {
     e.stopPropagation();
   });
 
@@ -29,20 +25,50 @@ $(function () {
       {
         scrollTop: position,
       },
-      300,
+      1000,
       "swing"
     );
     return false;
   });
 
-  //ページトップ
-  $("#js-page-top").on("click", function () {
-    $("body,html").animate(
-      {
-        scrollTop: 0,
-      },
-      300
-    );
-    return false;
-  });
+  //ページトップ:WIP!
+  // $("#js-page-top").on("click", function () {
+  //   $("body,html").animate(
+  //     {
+  //       scrollTop: 0,
+  //     },
+  //     1000
+  //   );
+  //   return false;
+  // });
+
+  // //ページ内ナビゲーション
+  // var menu = $("#js-gnav li a"),
+  //   menuLen = menu.length,
+  //   con = $(".section"),
+  //   conArr = [],
+  //   conArrLen,
+  //   pos;
+  // $(window).on("load resize", function () {
+  //   for (var i = 0; i < menuLen; i++) {
+  //     pos = con.eq(i).offset().top + 10;
+  //     conArr[i] = pos;
+  //   }
+  //   conArrLen = conArr.length;
+  // });
+
+  // $(window).on("load scroll", function () {
+  //   posCheck();
+  // });
+
+  // function posCheck() {
+  //   var scrollPos = $(window).scrollTop();
+  //   for (var i = 0; i < conArrLen; i++) {
+  //     if (conArr[i] <= scrollPos && scrollPos < conArr[i + 1]) {
+  //       menu.removeClass("is-current");
+  //       menu.eq(i).addClass("is-current");
+  //       break;
+  //     }
+  //   }
+  // }
 });
